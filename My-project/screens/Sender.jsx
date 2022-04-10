fe82e8f7c5925b4f25881d8b225fb9e43ec52828
JakubSilny <jakub.silny@pslib.cz>
2022-04-10 03:46:06 +0200

    commit

--- My-project/screens/Sender.jsx
+++ My-project/screens/Sender.jsx
@@ -3,10 +3,49 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert, Button } from 'react-n
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import * as SMS from "expo-sms"; 
-
+import * as Contacts from 'expo-contacts';
+import * as Permissions from 'expo-permissions';
 export const Sender =  props => {
   var numbers = ["+420732428076", "+420606032398"];
   const [SMSstatus, setSmsstatus] = useState(null)
+  const showFirstContactAsync = async () => {
+    // Ask for permission to query contacts.
+    const permission = await Permissions.askAsync(Permissions.CONTACTS);
+  
+    if (permission.status !== 'granted') {
+      // Permission was denied...
+      return;
+    }
+    else {
+      const contacts = await Contacts.getContactsAsync({
+      
+        fields: [
+          Contacts.PHONE_NUMBERS,
+          Contacts.EMAILS,
+        ], 
+        pageSize: 10,
+        pageOffset: 0,
+      });
+      try {
+        if (contacts.total > 0) {
+          Alert.alert(
+            'Your first contact is...',
+            `Name: ${contacts.data[0].name}\n` +
+            `Phone numbers: ${contacts.data[0].phoneNumbers[0].number}\n`
+            
+          );
+        }
+      } catch (error) {
+        console.log(error)
+        console.log(contacts.data[0].phoneNumbers[0].number)
+      }
+    }
+    
+    
+    
+  }
+  
+
   const smsSender = async () => {
     try {
       const status = await SMS.sendSMSAsync(
@@ -25,11 +64,12 @@ export const Sender =  props => {
   return (
     <View style={styles.container}>
       <Button onPress={smsSender} title="Send SMS" />
-
-
+      <Button onPress={showFirstContactAsync} title="Contacts" />
+         
       {SMSstatus !== null &&
         <View style={{ borderWidth: 2, borderColor: 'black', margin: 20, padding: 10 }}>
           <Text>{SMSstatus}</Text>
+         
         </View>
       }
       <StatusBar style="auto" />
